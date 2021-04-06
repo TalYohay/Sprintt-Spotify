db7f64bf896a8be3cda30651a538d1dcea8ddd13
TalYohay <talyohay@gmail.com>
2021-04-06 15:42:33 +0300

    added song toggle functionality

--- src/app/play-list-page/play-list-page.component.ts
+++ src/app/play-list-page/play-list-page.component.ts
@@ -1,109 +1,102 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import{ PlayListsService } from '../services/play-lists.service'
-import { Observable, forkJoin } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { PlayListsService } from "../services/play-lists.service";
+import { Observable, forkJoin } from "rxjs";
 
 @Component({
-  selector: 'app-play-list-page',
-  templateUrl: './play-list-page.component.html',
-  styleUrls: ['./play-list-page.component.css']
+  selector: "app-play-list-page",
+  templateUrl: "./play-list-page.component.html",
+  styleUrls: ["./play-list-page.component.css"]
 })
 export class PlayListPageComponent implements OnInit {
+  constructor(
+    public playListsAPI: PlayListsService,
+    public actRoute: ActivatedRoute
+  ) {}
 
-  constructor(public playListsAPI:PlayListsService, public actRoute: ActivatedRoute) { }
-
-  playListId:any
-  playListSongs:any = []
-  searchTerm:any;
-  playlists3:any=[]
-  selectedSong:any = {}
-  image_tracker:any = 'play'
-  selectedRow:any;
-  play:any = new Audio()
-  token:any = '1072694e-6a8b-4973-9cd0-96ac1ee6e4a2'
-
-  player = new Audio()
+  playListId: any;
+  playListSongs: any = [];
+  searchTerm: any;
+  playlists3: any = [];
+  selectedSong: any = {};
+  image_tracker: any = "play";
+  selectedRow: any;
+  token: any = "1072694e-6a8b-4973-9cd0-96ac1ee6e4a2";
+  player = new Audio();
 
   ngOnInit(): void {
-    this.playListId = this.actRoute.snapshot.params['id']
-    console.log('Play list ID:', this.playListId)
-    this.getPlaylistSongs()
-    this.combinAllAPI()
+    this.playListId = this.actRoute.snapshot.params["id"];
+    console.log("Play list ID:", this.playListId);
+    this.getPlaylistSongs();
+    this.combinAllAPI();
   }
 
-  getPlaylistSongs(){
-    this.playListsAPI.getSongsByPlaylistID(this.playListId).subscribe((data)=>{
-      this.playListSongs = data
-      console.log(this.playListSongs)
-    })
+  getPlaylistSongs() {
+    this.playListsAPI.getSongsByPlaylistID(this.playListId).subscribe(data => {
+      this.playListSongs = data;
+      console.log(this.playListSongs);
+    });
   }
 
-  combinAllAPI(){
+  combinAllAPI() {
     let req1 = this.playListsAPI.featuredPlaylist();
     let req2 = this.playListsAPI.MoodPlaylist();
-    forkJoin([req1,req2]).subscribe((data:Object[])=>{
+    forkJoin([req1, req2]).subscribe((data: Object[]) => {
       this.playlists3 = data.flat(1);
-      console.log(' this.playlists3:', this.playlists3)
-    })
-
+      console.log(" this.playlists3:", this.playlists3);
+    });
   }
 
-getSongInfo(song:any){
-this.selectedSong = song;
-console.log(this.selectedSong)
-}
-
-
-
-changeImg2(){
-  let image = <HTMLInputElement>document.getElementById('imgClickAndChange');
-  let image2 = <HTMLInputElement>document.getElementById('imgClickAndChange2');
-  if(this.image_tracker =='play'){
-    image.src = "../assets/pause_line_icon.png"
-    image2.src = "../assets/controller_icons/bar_pause.png"
-    this.image_tracker='pause'
-  }else{
-    image.src = "../assets/play_line_icon.png"
-    image2.src = "../assets/controller_icons/bar_play.png"
-    this.image_tracker='play'
+  getSongInfo(song: any) {
+    this.selectedSong = song;
+    console.log(this.selectedSong);
   }
-}
 
-setClickedRow(index:any){
-  this.selectedRow = index
-  console.log(this.selectedRow)
-  
-  let element = <HTMLInputElement>document.getElementById('imgClickAndChange');
-  element.classList.remove("playBtn");
-  element.classList.add("showBtn");
+  changeImg2() {
+    let image = <HTMLInputElement>document.getElementById("imgClickAndChange");
+    let image2 = <HTMLInputElement>(
+      document.getElementById("imgClickAndChange2")
+    );
+    if (this.image_tracker == "play") {
+      image.src = "../assets/pause_line_icon.png";
+      image2.src = "../assets/controller_icons/bar_pause.png";
+      this.image_tracker = "pause";
+    } else {
+      image.src = "../assets/play_line_icon.png";
+      image2.src = "../assets/controller_icons/bar_play.png";
+      this.image_tracker = "play";
+    }
+  }
 
-}
+  setClickedRow(index: any) {
+    this.selectedRow = index;
+    console.log(this.selectedRow);
 
-// playSong(){
-//   this.playListsAPI.playSong(this.selectedSong.track_id).subscribe((data)=>{
-//     this.play = data
-//     console.log('play:', this.play)
-//     // this.player.play()
-//   })
-  
-// }
-
-playSong(id:any){
-  this.playListsAPI.playSong(id)
-}
-
-pauseSong(id:any){
-  this.playListsAPI.pauseSong(id)
-}
-
-//  getEncryptedToken = (token:any='1072694e-6a8b-4973-9cd0-96ac1ee6e4a2') => {
-//   let date = new Date();
-//   let utcTime = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`
-//   let stringToEncrypt = `${token}===${utcTime}`
-//   console.log (btoa(stringToEncrypt))
-//   return btoa(stringToEncrypt)
-// }
+    let element = <HTMLInputElement>(
+      document.getElementById("imgClickAndChange")
+    );
+    element.classList.remove("playBtn");
+    element.classList.add("showBtn");
+  }
 
 
+  togglePlaystateSong(id: number) {
+    if (!this.selectedSong || this.selectedSong.track_id !== id) {
+      const token = this.playListsAPI.generateToken();
+      const songUrl = `http://api.sprintt.co/spotify/play/${id}?access=${token}`;
+      this.player.src = songUrl;
+      this.player.load();
+      this.player.play();
+    } else {
+      if (this.player.paused) {
+        this.player.play();
+      } else {
+        this.player.pause();
+      }
+    }
+  }
 
+  pauseSong() {
+    this.player.pause();
+  }
 }
--- src/app/services/play-lists.service.ts
+++ src/app/services/play-lists.service.ts
@@ -1,151 +1,84 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class PlayListsService {
+  url = "http://api.sprintt.co/spotify";
+  userToken = "1072694e-6a8b-4973-9cd0-96ac1ee6e4a2";
+  reqAudio: any;
 
-  url = 'http://api.sprintt.co/spotify'
-  userToken = '1072694e-6a8b-4973-9cd0-96ac1ee6e4a2'
-  reqAudio:any
+  constructor(public http: HttpClient) {}
 
-  songTracker='play'
+  featuredPlaylist() {
+    const token = "1072694e-6a8b-4973-9cd0-96ac1ee6e4a2";
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "user-access-token": token
+    });
 
-  constructor(public http:HttpClient) { }
+    const httpOptions = {
+      headers: headers
+    };
+    return this.http.get(
+      `https://cors.bridged.cc/http://api.sprintt.co/spotify/featured_playlists?limit=10`,
+      httpOptions
+    );
+  }
 
+  MoodPlaylist() {
+    const token = "1072694e-6a8b-4973-9cd0-96ac1ee6e4a2";
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "user-access-token": token
+    });
 
+    const httpOptions = {
+      headers: headers
+    };
+    return this.http.get(
+      `https://cors.bridged.cc/http://api.sprintt.co/spotify/mood_playlists?limit=10`,
+      httpOptions
+    );
+  }
 
-    featuredPlaylist(){
-      const token = '1072694e-6a8b-4973-9cd0-96ac1ee6e4a2'
-      let headers = new HttpHeaders({
-        'Content-Type': 'application/json',
-        'user-access-token': token
-      });
-      
-      const httpOptions = {
-        headers: headers
-      };
-      return this.http.get(`https://cors.bridged.cc/http://api.sprintt.co/spotify/featured_playlists?limit=10`, httpOptions);
-      };
+  RecentlyPlayed() {
+    const token = "1072694e-6a8b-4973-9cd0-96ac1ee6e4a2";
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "user-access-token": token
+    });
 
+    const httpOptions = {
+      headers: headers
+    };
+    return this.http.get(
+      `http://api.sprintt.co/spotify/recently_played_playlists?limit=10`,
+      httpOptions
+    );
+  }
 
-      MoodPlaylist(){
-        const token = '1072694e-6a8b-4973-9cd0-96ac1ee6e4a2'
-        let headers = new HttpHeaders({
-          'Content-Type': 'application/json',
-          'user-access-token': token
-        });
-        
-        const httpOptions = {
-          headers: headers
-        };
-        return this.http.get(`https://cors.bridged.cc/http://api.sprintt.co/spotify/mood_playlists?limit=10`, httpOptions);
-        };
-  
-        RecentlyPlayed(){
-          const token = '1072694e-6a8b-4973-9cd0-96ac1ee6e4a2'
-          let headers = new HttpHeaders({
-            'Content-Type': 'application/json',
-            'user-access-token': token
-          });
-          
-          const httpOptions = {
-            headers: headers
-          };
-          return this.http.get(`http://api.sprintt.co/spotify/recently_played_playlists?limit=10`, httpOptions);
-          };
+  getSongsByPlaylistID(id: any) {
+    const token = "1072694e-6a8b-4973-9cd0-96ac1ee6e4a2";
+    let headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "user-access-token": token
+    });
 
-          getSongsByPlaylistID(id:any){
+    const httpOptions = {
+      headers: headers
+    };
+    return this.http.get(
+      `http://api.sprintt.co/spotify/playlist_tracks/${id}`,
+      httpOptions
+    );
+  }
 
-            const token = '1072694e-6a8b-4973-9cd0-96ac1ee6e4a2'
-            let headers = new HttpHeaders({
-              'Content-Type': 'application/json',
-              'user-access-token': token
-            });
-            
-            const httpOptions = {
-              headers: headers
-            };
-            return this.http.get (`http://api.sprintt.co/spotify/playlist_tracks/${id}`, httpOptions)
-          }
-
-
-
-
-          // playSong(id:any){
-          //   const getEncryptedToken = (token:any=this.token) => {
-          //     let date = new Date();
-          //     let utcTime = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`
-          //     let stringToEncrypt = `${token}===${utcTime}`
-          //     return btoa(stringToEncrypt)
-          //   }
-          //   return this.http.get(`http://api.sprintt.co/spotify/play/${id}?access=${getEncryptedToken()}`)
-          // }
-
-          playSong(id:any){
-   
-            const getEncryptedToken = (token:any=this.userToken) => {
-               let date = new Date();
-               let utcTime = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`
-               let stringToEncrypt = `${token}===${utcTime}`
-               console.log(stringToEncrypt)
-               return btoa(stringToEncrypt)
-            }
-            //generate token
-            const token = getEncryptedToken();
-            //init mp3 stream
-            const audio = new Audio(`http://api.sprintt.co/spotify/play/${id}?access=${token}`);
-            // this.reqAudio = audio
-            console.log(audio)
-            // audio.play();
-            if(this.songTracker=="play"){
-              audio.play();
-              this.songTracker="pause"
-              console.log("Paused?:",audio.paused)
-            }else{
-              audio.pause();
-              this.songTracker="play"
-              console.log("Paused?:", audio.paused)
-            }
-
-
-            // if(audio.played){
-            //   audio.pause();
-            //   console.log('play:',audio.played)
-            // }else{
-            //   audio.play();
-            //   console.log("pause:",audio.paused)
-            // }
-            
-          
-
-            
-            
-         }
-
-         pauseSong(id:any){
-          const getEncryptedToken = (token:any='1072694e-6a8b-4973-9cd0-96ac1ee6e4a2') => {
-            let date = new Date();
-            let utcTime = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`
-            let stringToEncrypt = `${token}===${utcTime}`
-            console.log(stringToEncrypt)
-            return btoa(stringToEncrypt)
-         }
-         //generate token
-         const token = getEncryptedToken();
-         //init mp3 stream
-         const audio = new Audio(`http://api.sprintt.co/spotify/play/${id}?access=${token}`);
-         
-         //load new track and play it
-         
-         audio.pause();
-      }
-         }
-        
-
-      
-    
-  
-
-
+  generateToken() {
+    let date = new Date();
+    let utcTime = `${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`;
+    let stringToEncrypt = `${this.userToken}===${utcTime}`;
+    return btoa(stringToEncrypt);
+  }
+}
